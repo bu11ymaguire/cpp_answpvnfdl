2b0db126292f68e8e383dc316e4f969ade9ab70a
bu11ymaguire <134497208+bu11ymaguire@users.noreply.github.com>
2025-03-18 11:12:33 +0900

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+cin >> 은 기본적으로 스페이스바와 엔터에 대한 보정을 해준다.
+
+
+터미널
+3
+10 20 30
+➡ 
+
+int b;
+vector<int> x;
+
+
+for(i=0;i<n;i++){
+  cin << b;
+  x[i] =b;
+}
+
+
+
+*/
