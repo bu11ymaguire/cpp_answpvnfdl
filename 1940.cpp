b26435424ce89051d98802b66b4c3aee42b05ebe
bu11ymaguire <134497208+bu11ymaguire@users.noreply.github.com>
2025-03-29 15:19:14 +0900

    Create 1940.cpp
    주몽

---
+++ 1940.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int n; //재료의 개수
+  cin>>n;
+  int total; //두 재료의 합
+  cin>>total;
+  vector<int> jaeryo(n+1);
+  jaeryo[0]=0;
+  for(int i=1;i<=n;i++)
+  {
+    cin>>jaeryo[i];
+  }
+  int count =0; 
+  int i=1; 
+  while(i<n)
+  {
+    for(int j=i+1;j<=n;j++)
+    {
+        int sum = jaeryo[i];
+        sum += jaeryo[j];
+        if(sum==total)
+        {
+            count++;
+        }
+    }
+    i++;
+  }
+  cout<<count;
+  return 0;
+}
