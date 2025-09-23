cb144c4010e6a769860104c49ed1b6a6918a7ebd
bu11ymaguire <134497208+bu11ymaguire@users.noreply.github.com>
2025-09-23 12:08:24 +0900

    Increase dp vector size and adjust loop limits

--- 9657.cpp
+++ 9657.cpp
@@ -5,15 +5,15 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    vector<string> dp(101);
-    dp[1]="SK"; dp[2]="CY"; dp[3]="SK";dp[4]="Sk";
+    vector<string> dp(1001);
+    dp[1]="SK"; dp[2]="CY"; dp[3]="SK";dp[4]="SK";
 
-    for(int i = 5; i <=100; i++){
-        if(dp[i-4]=="CY"||dp[i-3]=="CY"||dp[i-1]=="CY") //상대방을 지게 할 수 있는 경우의 수가 한 개라도 있으면, 그 게임은 이길 수 있다고 간주.
+    for(int i = 5; i <=1000; i++){
+        if(dp[i-4]=="CY"||dp[i-3]=="CY"||dp[i-1]=="CY") //이기는 경우의 수가 하나라도 있으면 이긴다!
         {
             dp[i]="SK";
         }
-        else //이길 수 있는 경우의 수가 하나도 없으면, 패배.
+        else
         {
             dp[i]="CY";
         }
