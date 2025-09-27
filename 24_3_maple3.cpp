648f56f40a72e3c0de5d58e5e79f2abda6aa4cd2
bu11ymaguire <134497208+bu11ymaguire@users.noreply.github.com>
2025-09-27 15:05:24 +0900

    Implement main logic for price calculation

---
+++ 24_3_maple3.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N; long long m; 
+    cin >> N >> m;
+    m *=100;
+  /*
+  ratio = (one * two)/100 보다는
+  m *= 100이 더 낫다.
+  */
+    
+    long long result = LLONG_MAX;
+
+    while(N--)
+    {
+        long long one,two,price;
+        cin >> one >> two >> price;
+        if(one==0) continue;
+        if(price == 0 )
+        {
+           result = 0;
+           continue;
+        }
+        long long ratio = one * two;
+        long long now_price;
+        if(m%ratio==0){
+            now_price = (m/ratio) * price;
+        }else{
+            now_price = (m/ratio+1) * price;
+        }
+        if(result>now_price)
+        {
+            result = now_price;
+        }   
+    }
+    if(result==LLONG_MAX)
+    {
+        cout << -1;
+    }else{
+        cout << result;
+    }
+    return 0;
+}
