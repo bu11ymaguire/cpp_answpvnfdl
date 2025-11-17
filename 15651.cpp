9c6837dd9bd7ae05e9d71a010c54f55da77beab7
Jin-Uk Kim <134497208+bu11ymaguire@users.noreply.github.com>
2025-11-17 20:51:36 +0900

    Add DFS function for generating combinations
    Implement depth-first search (DFS) to generate combinations.

---
+++ 15651.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool visted[9];
+vector<int> arr;
+
+int n,m;
+
+void DFS(int hello)
+{
+    if(hello==m)
+    {
+        for(const auto& num: arr)
+        {
+            cout << num <<' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 1; i<=n; i++)
+    {
+        
+        arr.push_back(i);
+        DFS(hello+1);
+        
+        arr.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    DFS(0);
+    return 0;
+}
